ff08d5c6d6a7b9c4c2636adc291cd31cd5b262c0
Rolka93 <93rolandas@gmail.com>
2018-10-18 19:47:41 +0300

    NetBeans Commit

--- index.php
+++ index.php
@@ -1,12 +1,18 @@
-<?Php
-$distance = rand(0,300);
-$fuel_cons = 7.5; // Kiek litru sunaudojama 100 km.
-$fuel_price = 1.3;//Litro kaina
-$my_money = 100; //Kiek turime pinigu
-$sunaudos = $fuel_cons/100*$distance; //Kiek llitru reikia kelioneje
-$kaina =$sunaudos*$fuel_price;
-
-
-print "nuvaziavus $distance km,masina sunaudos $sunaudos l. visa tai kainuos $kaina ";
-
+<?php
+$x = 10;
+$y = 2;
+$p = "";
+for ($i = 0; $i < 10; $i++) {
+    $x += $y;
+    $p .= 'Dabartine $x verte: ' . $x . '<br>';
+}
 ?>
+<html lang="lt">
+    <head>
+    </head>
+    <body>
+        <p>
+            <?php print $p; ?>
+        </p>
+    </body>
+</html>

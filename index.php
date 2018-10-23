7e7112457aeaff21f75ae50c9788a0c633b9b3da
Rolka93 <93rolandas@gmail.com>
2018-10-23 12:39:09 +0300

    NetBeans Commit

--- index.php
+++ index.php
@@ -1,23 +1,40 @@
 <?php
-
-
-
-function add($x, $y) {
-
-    return $text = $x . ' * ' . $y . ' Product: ' . $x*$y;
-
-}
-
-
-
+$catalog = ['miltai'.'pienas'.'kiaušiniai'];
+$tekst =''; 
 ?>
-
+ <!DOCTYPE html>
 <html>
+<head>
+<style>
+.produktas {
+   width: 150px;
+   height: 150px; 
+}
+</style>
+</head>
 
-    <body>
+<body>
 
-        <?php print add(7,3); ?>
+<div class="produktas">
+  <h2>Miltai</h2>
+  <span class="">Kaina</span><br
+  <span class="">Aprašymas</span><br>
+  <span class="">Nuolaida</span><br>
+</div>
 
-    </body>
+<div class="produktas">
+  <h2>Pienas</h2>
+  <span class="">Kaina</span><br>
+  <span class="">Aprašymas </span><br>
+  <span class="">Nuolaida</span><br>
+</div>
 
-</html>
+<div class="produktas">
+  <h2>Kiaušiniai</h2> 
+  <span class="">Kaina</span><br>
+  <span class="">Aprašymas</span><br>
+  <span class="">Nuolaida</span><br>
+</div>
+    <p><?php print $tekst; ?></p>
+</body>
+</html> 
